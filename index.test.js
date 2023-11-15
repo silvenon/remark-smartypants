9173ba2250ad6995265728f833bb8709528d8fe8
Matija Marohnić <matija.marohnic@gmail.com>
2023-11-15 18:23:29 +0100

    Fix tests for higher versions of packages

--- index.test.js
+++ index.test.js
@@ -2,20 +2,18 @@ import { it, expect } from "vitest";
 import { remark } from "remark";
 import smartypants from "./";
 
-const { process } = remark().use(smartypants);
-
 it("implements SmartyPants", async () => {
-  const file = await process('# "Hello World!"');
+  const file = await remark().use(smartypants).process('# "Hello World!"');
   expect(String(file)).toMatchInlineSnapshot(`
     "# “Hello World!”
     "
   `);
 });
 
-it.todo("handles quotes around links", async () => {
-  const file = await process(
-    '"wow". go to "[google](https://www.google.com/)" today.',
-  );
+it.skip("handles quotes around links", async () => {
+  const file = await remark()
+    .use(smartypants)
+    .process('"wow". go to "[google](https://www.google.com/)" today.');
   expect(String(file)).toMatchInlineSnapshot(`
     "“wow”. go to “
     [google](https://www.google.com/)
